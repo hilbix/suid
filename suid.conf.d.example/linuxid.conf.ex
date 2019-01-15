b068fafe5a8667071dfd52d28c6ae8afd1a68a23
Valentin Hilbig <webmaster@scylla-charybdis.com>
2019-01-15 04:00:41 +0100

    suid.conf.d.example/linuxid.conf.ex added
    (cherry picked from commit 0bf7eb62c371a12c4ed3bc0ec32676b79c345bbd)

---
+++ suid.conf.d.example/linuxid.conf.ex
@@ -0,0 +1,17 @@
+# make && sudo make install
+# sudo ln -s linuxid.conf.ex /etc/suid.conf.d/linuxid.conf
+# make linuxid && sudo cp linuxid /tmp/linuxid && sudo cp linuxid /tmp/linuxid0 && sudo cp linuxid /tmp/linuxid1
+# sudo chown 0:0 /tmp/linuxid /tmp/linuxid0
+# sudo chown 1:1 /tmp/linuxid1
+# sudo chmod 755 /tmp/linuxid*
+# sudo chmod +s /tmp/linuxid[01]
+# /tmp/linuxid0
+# suid linuxid0a
+# suid linuxid0b
+# /tmp/linuxid1
+# suid linuxid1a
+# suid linuxid1b
+linuxid0a::0:0::/:suid:/tmp/linuxid
+linuxid0b::0:0::/:     /tmp/linuxid
+linuxid1a::1:1::/:suid:/tmp/linuxid
+linuxid1b::1:1::/:     /tmp/linuxid
