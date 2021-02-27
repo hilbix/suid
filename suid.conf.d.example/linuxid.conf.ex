c2b7da230124dbd452a920ee735fd4c863db560f
Valentin Hilbig <webmaster@scylla-charybdis.com>
2021-02-27 05:37:17 +0100

    suid.conf.d.example/linuxid.conf.ex was dangerous
    It used `/tmp/`.  After a reboot this directory is empty,
    so if somebody accidentally enabled this, this could lead to a
    problem.
    
    Note to self: Never put security related stuff into `/tmp/`.

--- suid.conf.d.example/linuxid.conf.ex
+++ suid.conf.d.example/linuxid.conf.ex
@@ -1,19 +1,21 @@
+# (no more really needed as everything is output with D flag now, use to verify)
 # make && sudo make install
-# sudo ln -s linuxid.conf.ex /etc/suid.conf.d/linuxid.conf
-# make linuxid && sudo cp linuxid /tmp/linuxid && sudo cp linuxid /tmp/linuxid0 && sudo cp linuxid /tmp/linuxid1
-# sudo chown 0:0 /tmp/linuxid /tmp/linuxid0
-# sudo chown 1:1 /tmp/linuxid1
-# sudo chmod 755 /tmp/linuxid*
-# sudo chmod +s /tmp/linuxid[01]
-# /tmp/linuxid0
+# sudo cp -i suid.conf.d.example/linuxid.conf.ex /etc/suid.conf.d/linuxid.conf
+# make linuxid
+# for a in '' 0 1; do sudo cp linuxid /linuxid$a; sudo chown 0:0 /linuxid$a; chmod 755 /linuxid$a; done
+# sudo chown 1:1 /linuxid1
+# sudo chmod +s  /linuxid[01]
+# /linuxid0
 # suid linuxid0a
 # suid linuxid0b
-# /tmp/linuxid1
+# suid linuxid0c
+# /linuxid1
 # suid linuxid1a
 # suid linuxid1b
-linuxid0a::0:0::/:suid:/tmp/linuxid
-linuxid0b::0:0::/:     /tmp/linuxid
-linuxid0c::0:0::/:root:/tmp/linuxid
-linuxid1a::1:1::/:suid:/tmp/linuxid
-linuxid1b::1:1::/:     /tmp/linuxid
-linuxid1c::1:1::/:root:/tmp/linuxid
+# suid linuxid1c
+linuxid0a::0:0:D:/:suid:/linuxid
+linuxid0b::0:0:D:/:     /linuxid
+linuxid0c::0:0:D:/:root:/linuxid
+linuxid1a::1:1:D:/:suid:/linuxid
+linuxid1b::1:1:D:/:     /linuxid
+linuxid1c::1:1:D:/:root:/linuxid
